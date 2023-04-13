8fef0060b074392a424f7a4aec60cf8d09bd9a12
sergio junca <sergio.junca@gmail.com>
2023-04-12 21:39:29 -0500

    feat: added images

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,12 @@ import React, { useRef, useState } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
+import home from "/home.png";
+import intro from "/intro.png";
+import live from "/live.png";
+import socials from "/socials.png";
+
+
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/effect-fade";
@@ -22,7 +28,7 @@ export default function App() {
         fadeEffect= { {
           crossFade: true
         }}
-        speed={1500}
+        speed={1100}
         // navigation={true}
         // pagination={{
         //   clickable: true,
@@ -30,13 +36,13 @@ export default function App() {
         modules={[EffectFade, Navigation]}
         className="mySwiper"
       >
-        <SwiperSlide className="swiper-slide-bg" style={{ backgroundImage: "url(https://swiperjs.com/demos/images/nature-1.jpg)" }}>
+        <SwiperSlide className="swiper-slide-bg" style={{ backgroundImage: `url(${intro})` }}>
         </SwiperSlide>
-        <SwiperSlide className="swiper-slide-bg" style={{ backgroundImage: "url(https://swiperjs.com/demos/images/nature-2.jpg)" }}>
+        <SwiperSlide className="swiper-slide-bg" style={{ backgroundImage:`url(${home})` }}>
         </SwiperSlide>
-        <SwiperSlide className="swiper-slide-bg" style={{ backgroundImage: "url(https://swiperjs.com/demos/images/nature-3.jpg)" }}>
+        <SwiperSlide className="swiper-slide-bg" style={{ backgroundImage: `url(${live})` }}>
         </SwiperSlide>
-        <SwiperSlide className="swiper-slide-bg" style={{ backgroundImage: "url(https://swiperjs.com/demos/images/nature-4.jpg)" }}>
+        <SwiperSlide className="swiper-slide-bg" style={{ backgroundImage: `url(${socials})` }}>
           <div style={{ backgroundColor: "rgba(0, 0, 0, 0.25)", color: 'white' }}>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor laoreet nisl, at hendrerit velit dignissim eu. In in ipsum lobortis, bibendum nibh eu, laoreet magna. Donec hendrerit sapien a lacinia commodo. Fusce maximus, leo non posuere euismod, tortor elit euismod elit, a ullamcorper metus nulla vel lorem. Ut vel placerat purus, ac vestibulum metus. Sed vel ornare ipsum. Aenean vel efficitur metus. Praesent
             </p>
